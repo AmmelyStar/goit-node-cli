2cd61835194211d5cbb60994c4c1ac9f305a68a3
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2024-02-08 14:26:47 +0100

    edit

---
+++ src/contacts.js
@@ -0,0 +1,33 @@
+const fs = require("node:fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+    // ...твій код. Повертає масив контактів.
+    const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+    
+    console.log(data)
+    return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+}
